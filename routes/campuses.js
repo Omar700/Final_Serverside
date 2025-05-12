0422adac4f538e65fda6177092ab02995654d5d3
Omar <omar.arafa51@myhunter.cuny.edu>
2025-05-11 23:37:07 -0400

    add delete get post and put both student and campuses real this time

--- routes/campuses.js
+++ routes/campuses.js
@@ -4,66 +4,65 @@
 It defines all the campuses-related routes.
 ==================================================*/
 // Import Express module
-const express = require('express');
-// Create an Express router function called "router"
+const express = require("express");
 const router = express.Router();
-// Import database models
-const { Student, Campus } = require('../database/models');
+const { Campus, Student } = require("../database/models");
 
-// Import a middleware to replace "try and catch" for request handler, for a concise coding (fewer lines of code)
-const ash = require('express-async-handler');
+// GET /api/campuses - get all campuses
+router.get("/", async (req, res, next) => {
+  try {
+    const campuses = await Campus.findAll();
+    res.json(campuses);
+  } catch (err) {
+    next(err);
+  }
+});
 
-/* GET ALL CAMPUSES: async/await using "try-catch" */
-// router.get('/', async (req, res, next) => {
-//   try {
-//     let campuses = await Campus.findAll({include: [Student]});
-//     res.status(200).json(campuses);
-//   } 
-//   catch(err) {
-//     next(err);
-//   }
-// });
+// GET /api/campuses/:id - get single campus with students
+router.get("/:id", async (req, res, next) => {
+  try {
+    const campus = await Campus.findByPk(req.params.id, {
+      include: Student,
+    });
+    if (campus) res.json(campus);
+    else res.status(404).send("Campus not found");
+  } catch (err) {
+    next(err);
+  }
+});
 
-/* GET ALL CAMPUSES */
-router.get('/', ash(async(req, res) => {
-  let campuses = await Campus.findAll({include: [Student]});  // Get all campuses and their associated students
-  res.status(200).json(campuses);  // Status code 200 OK - request succeeded
-}));
+// POST /api/campuses - create new campus
+router.post("/", async (req, res, next) => {
+  try {
+    const newCampus = await Campus.create(req.body);
+    res.status(201).json(newCampus);
+  } catch (err) {
+    next(err);
+  }
+});
 
-/* GET CAMPUS BY ID */
-router.get('/:id', ash(async(req, res) => {
-  // Find campus by Primary Key
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});  // Get the campus and its associated students
-  res.status(200).json(campus);  // Status code 200 OK - request succeeded
-}));
+// PUT /api/campuses/:id - update campus
+router.put("/:id", async (req, res, next) => {
+  try {
+    const campus = await Campus.findByPk(req.params.id);
+    if (!campus) return res.status(404).send("Campus not found");
 
-/* DELETE CAMPUS */
-router.delete('/:id', ash(async(req, res) => {
-  await Campus.destroy({
-    where: {
-      id: req.params.id
-    }
-  });
-  res.status(200).json("Deleted a campus!");
-}));
+    await campus.update(req.body);
+    res.json(campus);
+  } catch (err) {
+    next(err);
+  }
+});
 
-/* ADD NEW CAMPUS */
-router.post('/', ash(async(req, res) => {
-  let newCampus = await Campus.create(req.body);
-  res.status(200).json(newCampus);  // Status code 200 OK - request succeeded
-}));
+// DELETE /api/campuses/:id - delete campus
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deleted = await Campus.destroy({ where: { id: req.params.id } });
+    if (deleted) res.status(204).end();
+    else res.status(404).send("Campus not found");
+  } catch (err) {
+    next(err);
+  }
+});
 
-/* EDIT CAMPUS */
-router.put('/:id', ash(async(req, res) => {
-  await Campus.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  });
-  // Find campus by Primary Key
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});  // Get the campus and its associated students
-  res.status(201).json(campus);  // Status code 201 Created - successful creation of a resource
-}))
-
-// Export router, so that it can be imported to construct the apiRouter (app.js)
 module.exports = router;
