534383cdac8d2c21e96f71b0d9d1376b3bc2cbe4
Omar <omar.arafa51@myhunter.cuny.edu>
2025-05-12 23:58:09 -0400

    changed some variable names and added gpa image url and campus id to students. And added image url to campus

--- database/models/Student.js
+++ database/models/Student.js
@@ -6,17 +6,37 @@ It defines the student model for the database.
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
 
-const Student = db.define("student", {
-  firstname: {
+const Student = db.define('student', {
+  firstName: {
     type: Sequelize.STRING,
     allowNull: false
   },
 
-  lastname: {
+  lastName: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true  
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://placehold.co/200x200'
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL(3, 2),
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
   }
 });
 
-// Export the student model
 module.exports = Student;
